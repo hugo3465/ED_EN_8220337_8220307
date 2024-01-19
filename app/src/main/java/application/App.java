41c200e2bf4ebb2ccef23fa39c84bc6376232faa
Pedro Pinho <116769327+PedroPinho17@users.noreply.github.com>
2024-01-19 23:19:18 +0000

    Update App.java

--- app/src/main/java/application/App.java
+++ app/src/main/java/application/App.java
@@ -15,12 +15,13 @@ public class App {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int numBots;
+        int numBots = 0;
+        String botName = null;
 
         GameMap map = new GameMap("unidirecional");
 
         map.generateRandomMap(15, false, 0.60);
-        map.exportMap("C:\\Users\\User\\Desktop\\" + map.getMapName() + ".txt");
+        map.exportMap("C:\\Users\\pedro\\Desktop\\" + map.getMapName() + ".txt");
         int positionPlayer1 = 1;
         int positionPlayer2 = 12;
 
@@ -28,7 +29,8 @@ public class App {
         Flag flagPlayer2 = new Flag(positionPlayer2);
 
         do {
-            System.out.println("Quantos bots deseja[1-4]?");
+            System.out.println("AVISO: O número de bots é entre 1 e 4");
+            System.out.println("Quantos bots deseja [1-4] ?");
             numBots = in.nextInt();
         } while (numBots <= 0 || numBots >= 5);
 
@@ -38,127 +40,35 @@ public class App {
         System.out.println("Player 1:");
 
         for (int i = 0; i < numBots - 1; i++) {
-            botsPlayer1[i] = new Bot("John Mendes", null, flagPlayer2);
-
-            System.out.println("Escolha o algoritmo para o Bot " + (i + 1) + ":");
-            System.out.println("1. Shortest Path Algorithm");
-            System.out.println("2. Random Movement Algorithm");
-
-            int escolha = in.nextInt();
-            MovementAlgorithm<GameEntity> algorithm = null;
-
-            switch (escolha) {
-                case 1:
-                    algorithm = new ShortestPathAlgorithm(map);
-                    break;
-                case 2:
-                    //algorithm = new RandomMovementAlgorithm(map);
-                    break;
-                default:
-                    System.out.println("Escolha inválida. Usando o algoritmo padrão.");
-                    algorithm = new ShortestPathAlgorithm(map);
-            }
-
-            boolean algoritmoRepetido = false;
-            for (int j = 0; j < i; j++) {
-                if (botsPlayer1[j].getMovementAlgorithm().getClass() == algorithm.getClass()) {
-                    algoritmoRepetido = true;
-                    break;
-                }
-            }
-
-            if (algoritmoRepetido) {
-                System.out.println("Algoritmo repetido. Escolha novamente.");
-                i--;
-            } else {
-                botsPlayer1[i].setMovementAlgorithm(algorithm);
-            }
+            System.out.println("Qual o nome que você deseja para o Bot " + (i + 1) + "?");
+            botName = in.next(); // Solicita o nome do bot
+            MovementAlgorithm<GameEntity> algorithm = chooseAlgorithm(in, botsPlayer1, i, numBots);
+            botsPlayer1[i] = new Bot(botName, algorithm, flagPlayer2);
         }
 
-        System.out.println("Escolha o algoritmo para o último Bot do jogador 1:");
-        System.out.println("1. Shortest Path Algorithm");
-        System.out.println("2. Random Movement Algorithm");
+        // Solicita o nome do último bot do Player 1
+        System.out.println("Qual o nome que você deseja para o último Bot do Player 1?");
+        botName = in.next();
+        botsPlayer1[numBots - 1] = new Bot(botName, chooseAlgorithm(in, botsPlayer1, numBots - 1, numBots), flagPlayer2);
 
-        int escolhaUltimoBotPlayer1 = in.nextInt();
-        MovementAlgorithm<GameEntity> algorithmUltimoBotPlayer1 = null;
-
-        switch (escolhaUltimoBotPlayer1) {
-            case 1:
-                algorithmUltimoBotPlayer1 = new ShortestPathAlgorithm(map);
-                break;
-            case 2:
-                //algorithmUltimoBotPlayer1 = new RandomMovementAlgorithm(map);
-                break;
-            default:
-                System.out.println("Escolha inválida. Usando o algoritmo padrão.");
-                algorithmUltimoBotPlayer1 = new ShortestPathAlgorithm(map);
-        }
-
-        botsPlayer1[numBots - 1] = new Bot("John Mendes", algorithmUltimoBotPlayer1, flagPlayer2);
-
-        System.out.println("Qual o nome que voce deseja");
+        System.out.println("Qual o nome que você deseja para o Player 1");
         String nameplayer1 = in.next();
 
         Player player1 = new Player(nameplayer1, flagPlayer1.getPosition(), flagPlayer2.getPosition(), botsPlayer1);
 
+        System.out.println("Player 2:");
+
         for (int i = 0; i < numBots - 1; i++) {
-            botsPlayer2[i] = new Bot("Bot Player 2", null, flagPlayer1);
-
-            System.out.println("Escolha o algoritmo para o Bot " + (i + 1) + " do Player 2:");
-            System.out.println("1. Shortest Path Algorithm");
-            System.out.println("2. Random Movement Algorithm");
-
-            int escolha = in.nextInt();
-            MovementAlgorithm<GameEntity> algorithm = null;
-
-            switch (escolha) {
-                case 1:
-                    algorithm = new ShortestPathAlgorithm(map);
-                    break;
-                case 2:
-                    //algorithm = new RandomMovementAlgorithm(map);
-                    break;
-                default:
-                    System.out.println("Escolha inválida. Usando o algoritmo padrão.");
-                    algorithm = new ShortestPathAlgorithm(map);
-            }
-
-            boolean algoritmoRepetidoPlayer2 = false;
-            for (int j = 0; j < i; j++) {
-                if (botsPlayer2[j].getMovementAlgorithm().getClass() == algorithm.getClass()) {
-                    algoritmoRepetidoPlayer2 = true;
-                    break;
-                }
-            }
-
-            if (algoritmoRepetidoPlayer2) {
-                System.out.println("Algoritmo repetido. Escolha novamente.");
-                i--;
-            } else {
-                botsPlayer2[i].setMovementAlgorithm(algorithm);
-            }
+            System.out.println("Qual o nome que você deseja para o Bot " + (i + 1) + "?");
+            botName = in.next(); // Solicita o nome do bot
+            MovementAlgorithm<GameEntity> algorithm = chooseAlgorithm(in, botsPlayer2, i, numBots);
+            botsPlayer2[i] = new Bot(botName, algorithm, flagPlayer1);
         }
 
-        System.out.println("Escolha o algoritmo para o último Bot do jogador 2:");
-        System.out.println("1. Shortest Path Algorithm");
-        System.out.println("2. Random Movement Algorithm");
-
-        int escolhaUltimoBotPlayer2 = in.nextInt();
-        MovementAlgorithm<GameEntity> algorithmUltimoBotPlayer2 = null;
-
-        switch (escolhaUltimoBotPlayer2) {
-            case 1:
-                algorithmUltimoBotPlayer2 = new ShortestPathAlgorithm(map);
-                break;
-            case 2:
-                //algorithmUltimoBotPlayer2 = new RandomMovementAlgorithm(map);
-                break;
-            default:
-                System.out.println("Escolha inválida. Usando o algoritmo padrão.");
-                algorithmUltimoBotPlayer2 = new ShortestPathAlgorithm(map);
-        }
-
-        botsPlayer2[numBots - 1] = new Bot("Bot Player 2", algorithmUltimoBotPlayer2, flagPlayer1);
+        // Solicita o nome do último bot do Player 2
+        System.out.println("Qual o nome que você deseja para o último Bot do Player 2?");
+        botName = in.next();
+        botsPlayer2[numBots - 1] = new Bot(botName, chooseAlgorithm(in, botsPlayer2, numBots - 1, numBots), flagPlayer1);
 
         System.out.println("Qual o nome que você deseja para o Player 2?");
         String nameplayer2 = in.next();
@@ -169,5 +79,65 @@ public class App {
 
         // Iniciar o jogo
         game.startGame(player1, player2);
+    }
+
+    private static MovementAlgorithm<GameEntity> chooseAlgorithm(Scanner in, Bot[] bots, int botIndex, int numBots) {
+        MovementAlgorithm<GameEntity> algorithm = null;
+        boolean algoritmoRepetido;
+
+        // Verifica se todos os algoritmos já foram escolhidos
+        boolean allAlgorithmsChosen = true;
+        for (int i = 0; i < numBots; i++) {
+            if (bots[i] == null || bots[i].getMovementAlgorithm() == null) {
+                allAlgorithmsChosen = false;
+                break;
+            }
+        }
+
+        if (allAlgorithmsChosen) {
+            System.out.println("Todos os algoritmos já foram escolhidos. Não é possível escolher mais.");
+            return null;
+        }
+
+        do {
+            algoritmoRepetido = false; // Reinicializa algoritmoRepetido no início do loop
+
+            System.out.println("Escolha o algoritmo para o Bot " + (botIndex + 1) + ":");
+            System.out.println("1. Shortest Path Algorithm");
+            System.out.println("2. Random Movement Algorithm");
+
+            int escolha = in.nextInt();
+
+            switch (escolha) {
+                case 1:
+                    algorithm = new ShortestPathAlgorithm(new GameMap("unidirecional"));
+                    break;
+                case 2:
+                    // algorithm = new RandomMovementAlgorithm(new GameMap("unidirecional"));
+                    break;
+                default:
+                    System.out.println("Escolha inválida. Tente novamente.");
+                    continue; // Volta para o início do loop sem verificar algoritmo repetido
+            }
+
+            // Verifica se o algoritmo escolhido já foi escolhido por algum bot anterior
+            for (int j = 0; j < botIndex; j++) {
+                if (bots[j] != null && bots[j].getMovementAlgorithm() != null
+                        && bots[j].getMovementAlgorithm().getClass() == algorithm.getClass()) {
+                    algoritmoRepetido = true;
+                    System.out.println("Algoritmo repetido. Escolha novamente.");
+                    break;
+                }
+            }
+
+            // Se o bot for o 4º (índice 3) e o número total de bots for 4, permite qualquer
+            // algoritmo
+            if (botIndex == 3 && numBots == 4) {
+                break;
+            }
+
+        } while (algorithm == null || algoritmoRepetido);
+
+        return algorithm;
     }
 }
